e042e8040b116a20d085630a0542af0746147f43
Mackenzie Zastrow <zastrowm+github@gmail.com>
2014-11-10 21:59:29 -0600

    Rotate at a more fixed rate.

--- Assets/NineByteGames.TowerDefense/Utils/MathUtils.cs
+++ Assets/NineByteGames.TowerDefense/Utils/MathUtils.cs
@@ -7,8 +7,13 @@ namespace NineByteGames.TowerDefense.Utils
 {
   internal class MathUtils
   {
-    public const float TicksPerSecond = 30f;
+    /// <summary> The maximum number of degrees to turn per iteration. </summary>
+    public const float MaxDegreesPerSecond = 30;
 
+    /// <summary> The number of ticks that occur. </summary>
+    public const float TicksPerSecond = 50f;
+
+    /// <summary> The number of seconds in a tick. </summary>
     public const float SecondsPerTick = 1.0f / TicksPerSecond;
 
     /// <summary> Convert a number of ticks into a timespan. </summary>
@@ -21,11 +26,12 @@ namespace NineByteGames.TowerDefense.Utils
 
     /// <summary>
     ///  2D rotate one object to face another.  It aligns the top of the object to be looking at the
-    ///  other object's center.
+    ///  other object's center, not rotating more than a fixed amount per call.
     /// </summary>
     /// <param name="objectToRotate"> The object to rotate. </param>
     /// <param name="target"> The target to rotate towards. </param>
-    /// <param name="rateToTurn"> The time to use to interpolate the current rotation to the new rotation.. </param>
+    /// <param name="rateToTurn"> The time to use to interpolate the current rotation to the new
+    ///  rotation. </param>
     public static void RotateTowards(GameObject objectToRotate, GameObject target, float rateToTurn)
     {
       var targetTransform = target.transform;
@@ -35,35 +41,11 @@ namespace NineByteGames.TowerDefense.Utils
       Vector3 vectorToTarget = targetTransform.position - objectTransform.position;
 
       // convert it into an engle
-      float desiredAngle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) - Mathf.PI / 2;
+      float desiredAngle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) - Mathf.PI / 2) * Mathf.Rad2Deg;
 
-      // then a Quaternion
-      Quaternion desiredQuaternion = Quaternion.AngleAxis(desiredAngle * Mathf.Rad2Deg, Vector3.forward);
-
-      // finally rotate it
-      objectTransform.rotation = Quaternion.Slerp(objectTransform.rotation, desiredQuaternion, rateToTurn);
-    }
-
-    private static void RotateTowards2(GameObject objectToRotate, GameObject target, float rateToTurn)
-    {
-      var targetTransform = target.transform;
-      var objectTransform = objectToRotate.transform;
-
-      // get the desired vector rotation
-      Vector3 vectorToTarget = targetTransform.position - objectTransform.position;
-
-      // convert it into an engle
-      float desiredAngle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) - Mathf.PI / 2;
-      desiredAngle *= Mathf.Rad2Deg;
-
-      float currentAngle = objectTransform.eulerAngles.z;
-
-      // then a Quaternion
-      Quaternion desiredQuaternion = Quaternion.AngleAxis(Mathf.LerpAngle(currentAngle, desiredAngle, rateToTurn),
-                                                          Vector3.forward);
-
-      // finally rotate it
-      objectTransform.rotation = desiredQuaternion;
+      objectTransform.rotation = Quaternion.RotateTowards(objectTransform.rotation,
+                                                          Quaternion.AngleAxis(desiredAngle, Vector3.forward),
+                                                          MaxDegreesPerSecond * rateToTurn);
     }
   }
 }
