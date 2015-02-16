9118c4d79a76edd0b4cf4c3b58fd4e5d6f109c65
Mackenzie Zastrow <zastrowm+github@gmail.com>
2015-02-16 12:58:48 -0600

    Have enemies damage the player when in range

---
+++ Assets/NineByteGames.TowerDefense/Behaviors/DamageOnTriggerBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NineByteGames.TowerDefense.Messages;
+using NineByteGames.TowerDefense.Signals;
+using NineByteGames.TowerDefense.Unity;
+using NineByteGames.TowerDefense.Utils;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace NineByteGames.TowerDefense.Behaviors
+{
+  /// <summary> Add damage to the objects that trigger the collider on this object. </summary>
+  [RequireComponent(typeof(Collider2D))]
+  internal class DamageOnTriggerBehavior : AttachedBehavior
+  {
+    [Tooltip("The layers on which to cause damage")]
+    public LayerMask layerMask;
+
+    [Tooltip("The amount of damage to do to touched entities")]
+    public float damage;
+
+    [Tooltip("How fast damage occurs on a given target")]
+    [FormerlySerializedAs("attackLimiter")]
+    public RateLimiter rateOfAttack;
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+      if (!rateOfAttack.CanTrigger)
+        return;
+
+      var target = other.gameObject;
+      if (!layerMask.Contains(target))
+        return;
+
+      target.SendSignal(new Damage(this.damage));
+      rateOfAttack.Restart();
+    }
+  }
+}
---
+++ Assets/NineByteGames.TowerDefense/Unity/LayerMaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace NineByteGames.TowerDefense.Unity
+{
+  public static class LayerMaskExtensions
+  {
+    /// <summary> Check if a game object matches the given layer mask. </summary>
+    /// <param name="layerMask"> The layerMask to act on. </param>
+    /// <param name="gameObject"> The game object to check. </param>
+    /// <returns>
+    ///  true if the game object is on one of the layers in <paramref name="layerMask"/>, false if not.
+    /// </returns>
+    public static bool Contains(this LayerMask layerMask, GameObject gameObject)
+    {
+      return Layer.FromLayerId(gameObject.layer).IsIn(layerMask);
+    }
+  }
+}
