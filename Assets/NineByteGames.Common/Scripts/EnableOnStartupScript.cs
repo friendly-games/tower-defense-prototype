d17d2807c428d35a98368fad2cbf816bcc0af130
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2015-08-09 18:33:22 -0500

    Disable terrain by default, have script enable it on start

---
+++ Assets/NineByteGames.Common/Scripts/EnableOnStartupScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NineByteGames.Common.Scripts
+{
+  /// <summary> Enables a series of objects on startup. </summary>
+  public class EnableOnStartupScript : MonoBehaviour
+  {
+    [Tooltip("Items to enable on startup")]
+    public GameObject[] m_Objects;
+
+    // Use this for initialization
+    public void Start ()
+    {
+      foreach (var go in m_Objects)
+      {
+        go.SetActive(true);
+      }
+    }
+  }
+}
